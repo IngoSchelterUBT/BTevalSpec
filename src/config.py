Added calculation of osciStrength for multifit & BugFix. 	modified:   src/config.py 	modified:   src/eval.py 	modified:   src/handleTrace.py 	modified:   src/inout.py 	modified:   src/specFit.py 	modified:   src/specGuess.py

src/config.py
@@ -79,12 +79,12 @@ class Config:
       self.osciStrengths = np.array([])
       self.phases = np.array([])
       self.amplitudes = [] #amplitudes as list of numpy arrays
-      self.fix = []
+      self.fix = np.array([])
       for i in range(len(excitations)):
         self.names.append(excitations[i].get('name','none'))
         self.energies = np.append(self.energies,excitations[i].get('energy'))
         self.osciStrengths = np.append(self.osciStrengths,excitations[i].get('strength',0.0))
-        self.fix.append(excitations[i].get('fix',False))
+        self.fix = np.append(self.fix,excitations[i].get('fix',False))
         #self.phases = np.append(self.phases,excitations[i].get('phase'))
         for j in range(numDipoleFiles):
           self.amplitudes.append(excitations[i].get('amplitude_file%i' % (j+1),np.array([])))

src/handleTrace.py
@@ -22,17 +22,22 @@ def guessTrace(guessTrace, fit):
   #calculate relative error between the excitaion energies of the fits with boost
   #in x-, y- and z-direction
   w_trace = fit[0].fit_result[:,0]
-  #appearance_matrix = np.full((len(w_trace),len(fit)),False)
-  #appearance_matrix[:,0] = True
+  fix = fit[0].fix
   for i in range(1,len(fit)):
     for j in range(len(fit[i].fit_result[:,0])):
       err = np.abs(w_trace - fit[i].fit_result[j,0])/((w_trace + fit[i].fit_result[j,0])/2.)
       if np.any(err < 0.01): #0.01 equals a relative error of 1.0 %
         continue
       else:
+        print('line added:')
+        print(fit[i].fit_result[j,0])
+        print(i)
         w_trace = np.append(w_trace, fit[i].fit_result[j,0])
+        fix = np.append(fix, fit[i].fix[j])
 
-  w_trace = np.sort(w_trace)
+  arginds = w_trace.argsort()
+  w_trace = w_trace[arginds]
+  fix = fix[arginds]
 
   for i in range(len(fit)):
     fit[i].lineId = np.full(len(fit[i].fit_result[:,0]),-1,dtype=np.int32)
@@ -49,5 +54,6 @@ def guessTrace(guessTrace, fit):
 
   #save in guess for trace
   guessTrace.guess = np.column_stack([w_trace,f_trace])
+  guessTrace.fix = fix
     
   

src/inout.py
@@ -4,6 +4,7 @@ import numpy as np
 import shutil
 import os
 import ruamel.yaml
+import re
 
 #own modules
 import errorHandler as err
@@ -17,7 +18,7 @@ def writeEmptyConfig():
                       'PadeApprox' : {  'pade' : False, 'pade_wmax' : 0.6, 'pade_dw' : 0.00001, 'pade_smooth' : 0.0000001,
                                         'pade_thin' : 0
                                      },
-                      'FitSpectrum' : { 'fit' : False, 'fit_guess' : False, 'fit_relerr_crit' : 1.0e-12, 'fit_max_iter' : 1,'fit_range' : [0.1, 0.6], 'guess_thres' : 0.1},
+                      'FitSpectrum' : { 'fit' : False, 'fit_guess' : False, 'fit_relerr_crit' : 0.1, 'fit_max_iter' : 10,'fit_range' : [0.1, 0.6], 'guess_thres' : 0.1},
                     },
             'SPEC' : { 'nex' : 2,
                        'excitations' : [{'name' : 'S1', 'fix' : False, 'energy' : 0.2, 'strength' : 0.1, 'phase' : 0.05,
@@ -39,7 +40,6 @@ def readConfig():
 
 #Routine for writing excitation information
 #fit is a list of all fits (x-, y- and z-direction and trace)
-#TODO: Add a fix boolean
 #TODO: Change boolean guess=True to guess=False
 def writeExcitations(conf,fit): #attention conf is the object!!
   #read yaml file again
@@ -57,14 +57,20 @@ def writeExcitations(conf,fit): #attention conf is the object!!
     for i in range(len(fit[3].fit_result[:,0])):
       #create empty dictionary
       exc = {}
-      if conf.fit_guess or conf.excitations.names[i] == 'none':
+      try:
+        name_none = conf.excitations.names[i] == 'none'
+        name_S = re.match(r"S[0-9]+",conf.excitations.names[i])
+      except Exception:
+        name_none = True
+        name_S = False
+      if conf.fit_guess or name_none or name_S:
         exc['name'] = 'S%i' % (i+1)
       else:
-        exc['name'] = conf.excitations.names[i]
+        exc['name'] = str(conf.excitations.names[i])
       if conf.fit_guess:
         exc['fix'] = False
       else:
-        exc['fix'] = conf.excitations.fix[i]
+        exc['fix'] = bool(fit[3].fix[i])
       exc['energy'] = float(fit[3].fit_result[i,0])
       exc['strength'] = float(fit[3].fit_result[i,1])
       for j in range(3):
@@ -79,16 +85,23 @@ def writeExcitations(conf,fit): #attention conf is the object!!
     config['SPEC']['nex'] = len(fit[0].fit_result[:,0])
     for i in range(len(fit[0].fit_result[:,0])):
       exc = {}
-      if conf.fit_guess or conf.excitations.names[i] == 'none':
+      try:
+        name_none = conf.excitations.names[i] == 'none'
+        name_S = re.match(r"S[0-9]+",conf.excitations.names[i])
+      except Exception:
+        name_none = True
+        name_S = False
+      if conf.fit_guess or name_none or name_S:
         exc['name'] = 'S%i' % (i+1)
       else:
-        exc['name'] = conf.excitations.names[i]
+        exc['name'] = str(conf.excitations.names[i])
       if conf.fit_guess:
         exc['fix'] = False
       else:
-        exc['fix'] = conf.excitations.fix[i]
+        exc['fix'] = bool(fit[0].fix[i])
       exc['name'] = 'S%i' % (i+1)
       exc['energy'] = float(fit[0].fit_result[i,0])
+      exc['strength'] = float(fit[0].osciStrength[i])
       exc['amplitudes_file1'] = np.ndarray.tolist(fit[0].fit_result[i,1:])
       excitations.append(exc)
 

src/specFit.py
@@ -20,6 +20,7 @@ from pprint import pprint
 # The Class consists of the following variables:
 # - fitId: Id of the file that is fitted (starting with 0).
 # - propTime: Propagation time of the BTDFT calculation.
+# - kvec: k-vector of excitation
 # - fit_relerr_crit: relative error criterium for fit read out of eval.yaml
 # - fit_max_iter: Maxiumum number of iterations of the fit. Per iteration one
 #   additional line is added in the spectrum.
@@ -33,7 +34,8 @@ from pprint import pprint
 # - fit_relerr: Relative error between fit and data.
 # - osci_range: Range of fitted spectrum of cutted osci file.
 # - fit_result: Result of fitted spectrum. First column energies, then
-#   amplitudes: 1 column for trace, 3 columns for multifit)
+#   amplitudes: 1 column for trace (equals oscillator strength), 3 columns for multifit)
+# - osciStrength: For multifit calculated oscillator strength
 # - lineId (added in handleTrace.py): Only for multifit. Labels which line in
 #   the multifit contains to which line in the fit of the trace.
 #------------------------------------------------------------------------------#
@@ -44,6 +46,8 @@ class Fit:
     self.fitId = Id
     #read propagation time out of fourierTransform
     self.propTime = ft.propTime
+    #read k-Vector out of ft
+    self.kvec = ft.kvec
     #read relative error criterium out of config file
     self.fit_relerr_crit = config.fit_relerr_crit
     #read maximum of fit iterations out of config file
@@ -57,9 +61,12 @@ class Fit:
     #read fix booleans, if line should be fixed or not
     #if it is not configured than set fix to 0
     try:
-      self.fix = config.excitations.fix
+      if calcFlag == 'no':
+        self.fix = config.excitations.fix
+      elif calcFlag == 'trace':
+        self.fix = guess.fix
     except Exception:
-      self.fix = [False]*len(self.guess[:,0])
+      self.fix = np.full(len(self.guess[:,0]),False)
     #calculate dw as the frequency step
     self.dw = (self.osci[0][len(self.osci[0][:,0])-1,0]-self.osci[0][0,0]) /\
               float((len(self.osci[0][:,0])-1))
@@ -92,6 +99,8 @@ class Fit:
       if calcFlag == 'trace':
         print('Trace is fitted without iteration!')
         break
+      elif calcFlag == 'no':
+        self.osciStrength = self.calcOsciStrength()
 
       #stop if error criterium is fullfilled, else add a new line at maximum deviation
       #between fit and raw data
@@ -103,6 +112,10 @@ class Fit:
         #maxiumum deviation between fit and raw data
         self.addNewLine()
 
+    #sort fit_result after energies and fix corresponding to it
+    arginds = self.fit_result[:,0].argsort()
+    self.fit_result = self.fit_result[arginds]
+    self.fix = self.fix[arginds]
 
     # 2.) Plot the result of the fit, the raw data and deviation (is done later, not parallel)
     #self.plotFit(calcFlag)
@@ -234,7 +247,21 @@ class Fit:
     self.guess = np.vstack((self.guess,newLine))
     self.guess = self.guess[self.guess[:,0].argsort()]
     #also append a new fix boolean to the List
-    self.fix.append(False)
+    self.fix = np.append(self.fix,False)
+
+  #Routine for calculating oscillator stregth for multifit
+  def calcOsciStrength(self):
+    e_k = self.kvec/(np.linalg.norm(self.kvec))
+
+    #f is a numpy array for oscillator strength of every excitation line
+    f = np.array([])
+    for i in range(len(self.fit_result[:,0])):
+      e_i = self.fit_result[i,1:]/(np.linalg.norm(self.fit_result[i,1:]))
+      f_i = self.fit_result[i,1]/(np.vdot(e_k,e_i)*self.fit_result[i,1])
+      f = np.append(f,f_i)
+    
+    return f
+
 
 #-----------------------------------------------------------------------------#
 #   Methods for Making Plot

src/specGuess.py
@@ -59,8 +59,8 @@ class Guess:
       self.guess[:,0] = config.excitations.energies
       #read guess out of config-file
       if calcFlag == 'no':
-        #TODO: - read a1 oder a2 oder a3 as guess for fit
-        #      - for new added line search in Osci for guess of amplitude
+        #- read a1 oder a2 oder a3 as guess for fit
+        #- for new added line search in Osci for guess of amplitude
         for i in range(len(config.excitations.energies)):
           if len(config.excitations.amplitudes[i]) == 0:
             #new added line -> search in Osci for guess of amplitude
@@ -72,8 +72,8 @@ class Guess:
             self.guess[i,1:] = config.excitations.amplitudes[i]
       elif calcFlag == 'trace':
         #read oscillator strength as guess
-        #TODO: - read strength
-        #      - for new added line search in Osci for guess of strength
+        #- read strength
+        #- for new added line search in Osci for guess of strength
         for i in range(len(config.excitations.energies)):
           if config.excitations.osciStrengths[i] == 0.0:
             strength = self.searchAmplitude(np.array([config.excitations.energies[i]]))