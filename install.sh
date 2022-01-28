fc40a6d90670c0f05fc1bdebb59a3da2dad580d8
Rian Richter <rian.richter@uni-bayreuth.de>
2022-01-28 18:26:22 +0100

    Gitfix

--- install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#python3 -m PyInstaller --onefile eval.py
-pyinstaller --onefile --hidden-import [numpy,numpy.distutils] eval.py
-#pyinstaller --additional-hooks-dir=hooks --onefile eval.py
