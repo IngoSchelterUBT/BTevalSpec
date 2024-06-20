ce13a2d51a74ad0b77fb612c83036bbc7476ce2b
Ingo Schelter <bt302536@l1.localdomain>
2024-06-20 14:15:20 +0200

    Some bugfixes with transition-density decoupling

---
+++ src/BTclust.py
@@ -0,0 +1,1 @@
+../../compact/BTclust.py
--- src/transdens.py
+++ src/transdens.py
@@ -25,7 +25,7 @@ def decouple(densft,densen,excit,T,Ef,Ep,Hw,jcalc=0,dbg=0,imagonly=False):
     if any(nex!=nn for nn in [len(densft),len(densen)]): err.err(2,"Number of densities and energies must match number of excitations")
 
     # Compute matrix
-    b = np.zeros((nex,nex))
+    b = np.zeros((nex,nex),dtype=np.cdouble)
     for iex, ex in enumerate(excit.exlist):
         c= -Ef*np.dot(Ep,ex.dipole)*np.abs(Hw[iex])#*1/hbar, which is one in Ry a.u.
         for ien, en in enumerate(densen):
@@ -76,8 +76,8 @@ def getDipole(dens,org,dxyz):
         for ix in range(nn[1]):
             for iy in range(nn[2]):
                 for iz in range(nn[3]):
-                    dip[n,0] += dens[ix,iy,iz]*(org[0]+ix*dxyz[0])
-                    dip[n,1] += dens[ix,iy,iz]*(org[1]+iy*dxyz[1])
-                    dip[n,2] += dens[ix,iy,iz]*(org[2]+iz*dxyz[2])
+                    dip[n,0] += dens[n,ix,iy,iz]*(org[0]+ix*dxyz[0])
+                    dip[n,1] += dens[n,ix,iy,iz]*(org[1]+iy*dxyz[1])
+                    dip[n,2] += dens[n,ix,iy,iz]*(org[2]+iz*dxyz[2])
     return dip
 
